fd4acb63efe4d84d675f85b5e7274d2ed5e4d561
Even <kiki44552002@gmail.com>
2015-12-11 21:51:55 +0800

    add member migration

---
+++ migrations/20151208164116-members.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+exports.up = function(db, callback) {
+  const filePath = path.join(__dirname, '/sqls/20151208164116-members-up.sql');
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (err) return callback(err);
+      console.log('received data: ' + data);
+
+    db.runSql(data, function(err) {
+      if (err) return callback(err);
+      callback();
+    });
+  });
+};
+
+exports.down = function(db, callback) {
+  const filePath = path.join(__dirname, '/sqls/20151208164116-members-down.sql');
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (err) return callback(err);
+      console.log('received data: ' + data);
+
+    db.runSql(data, function(err) {
+      if (err) return callback(err);
+      callback();
+    });
+  });
+};
---
+++ migrations/sqls/20151208164116-members-down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS emails;
+DROP TABLE IF EXISTS members;
